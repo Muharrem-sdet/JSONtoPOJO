ac7d809be8e470accba1e198d8ca958486dfabb8
Muharrem <muharremustun@gmail.com>
2021-01-15 23:30:40 -0500

    User deserialization corrected

--- app/src/main/java/com/example/gsonsample/model/user/User.java
+++ app/src/main/java/com/example/gsonsample/model/user/User.java
@@ -1,17 +1,79 @@
 package com.example.gsonsample.model.user;
 
-import java.util.List;
 import com.google.gson.annotations.SerializedName;
 
-public class User{
+public class User {
 
-	private List<UsersItem> users;
+	private String website;
+	private Address address;
+	private String phone;
+	private String name;
+	private Company company;
+	private int id;
+	private String email;
+	private String username;
 
-	public void setUsers(List<UsersItem> users){
-		this.users = users;
+	public void setWebsite(String website){
+		this.website = website;
 	}
 
-	public List<UsersItem> getUsers(){
-		return users;
+	public String getWebsite(){
+		return website;
+	}
+
+	public void setAddress(Address address){
+		this.address = address;
+	}
+
+	public Address getAddress(){
+		return address;
+	}
+
+	public void setPhone(String phone){
+		this.phone = phone;
+	}
+
+	public String getPhone(){
+		return phone;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setCompany(Company company){
+		this.company = company;
+	}
+
+	public Company getCompany(){
+		return company;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	public void setUsername(String username){
+		this.username = username;
+	}
+
+	public String getUsername(){
+		return username;
 	}
 }
